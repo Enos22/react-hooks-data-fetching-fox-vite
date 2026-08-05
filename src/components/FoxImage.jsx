ff3d4453b29943197140f5c248444dd0bf661552
Emos Arenga <enos.arenga@gmail.com>
2026-08-05 20:04:34 +0300

    Fetch fox image on mount using useEffect

--- src/components/FoxImage.jsx
+++ src/components/FoxImage.jsx
@@ -1,11 +1,20 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import foxLogo from '../assets/fox-logo.png'
 
 const API_URL = "https://randomfox.ca/floof/"
 
 function FoxImage() {
   const [image, setImage] = useState(foxLogo)
-  
+
+  useEffect(() => {
+    //
+    fetch(API_URL)
+      .then((response => response ? "Network Error," : response.json()))
+      .then((data) => setImage(data.image))
+      .then(error => console.error('Error fetching image:', error));
+  }, []);
+
+
   return (
     <div>
       <p>Learn more about us!</p>
